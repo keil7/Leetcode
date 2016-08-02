e4c3e5e252ec2a5359494a89aeb069b6ee5cd393
keil7 <mcspro163@gmail.com>
2016-08-02 16:18:24 +0800

    Update Q151.cpp

--- Q151.cpp
+++ Q151.cpp
@@ -35,3 +35,42 @@ public:
         }
     }
 };
+
+class SolutionOptim {
+public:
+    void reverseWords(string &s) {
+        if(s.empty())
+            return;
+        //pre-processing
+        int index1 = 0;
+        int index2 = 0;
+        
+        while(index2<s.length()) {
+            if(index1!=0)
+                s[index1++] = ' ';
+            int wordStartIndex = index1;
+            while(s[index2]!=' '&&index2<s.length()) {
+                s[index1++] = s[index2++];
+            }
+            if(index1!=wordStartIndex)
+                reverseWord(s,wordStartIndex,index1-1);
+            while(s[index2]==' '&&index2<s.length()) {
+                ++index2;
+            }
+        }
+        s.erase(index1);
+        reverseWord(s,0,s.length()-1);
+    }
+    
+private:
+    void reverseWord(string &s,int index1,int index2) {
+        char temp;
+        while(index1<index2) {
+            temp = s[index1];
+            s[index1] = s[index2];
+            s[index2] = temp;
+            ++index1;
+            --index2;
+        }
+    }
+};
