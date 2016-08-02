f1e08cc4304666664adb73b426735dd7fedc601e
keil7 <mcspro163@gmail.com>
2016-08-02 15:32:37 +0800

    Create Q151.cpp

---
+++ Q151.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void reverseWords(string &s) {
+        if(s.empty())
+            return;
+        //pre-processing
+        int index1 = 0;
+        int index2 = 0;
+        string word;
+        while(index2<s.length()) {
+            word.clear();
+            if(index1!=0)
+                s[index1++] = ' ';
+            while(s[index2]!=' '&&index2<s.length()) {
+                word += s[index2++];
+            }
+            while(!word.empty()) {
+                s[index1++] = word.back();
+                word.pop_back();
+            }
+            while(s[index2]==' '&&index2<s.length()) {
+                ++index2;
+            }
+        }
+        s.erase(index1);
+        char temp;
+        index1=0;
+        index2=s.length()-1;
+        while(index1<index2) {
+            if(s[index1])
+            temp = s[index1];
+            s[index1] = s[index2];
+            s[index2] = temp;
+            ++index1;
+            --index2;
+        }
+    }
+};
