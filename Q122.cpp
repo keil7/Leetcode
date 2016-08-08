bffe5c49221d9729599c15e0c78ebf78ecdbd3e8
keil7 <mcspro163@gmail.com>
2016-08-08 17:19:13 +0800

    Create Q122.cpp

---
+++ Q122.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty())
+            return 0;
+        int profit = 0;
+        int totalProfit = 0;
+        int buyPrice = INT_MAX;
+        
+        for(int i=0;i<prices.size();++i) {
+            buyPrice = min(buyPrice,prices[i]);
+            if(profit>prices[i]-buyPrice) {
+                totalProfit += profit;
+                buyPrice = prices[i];
+                profit = 0;
+            }
+            else
+                profit = prices[i]-buyPrice;
+        }
+        
+        totalProfit += profit;
+        return totalProfit;
+    }
+};
