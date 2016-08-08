a2be201d1b34f37dc07e9ce68c8f14d35bbb0569
keil7 <mcspro163@gmail.com>
2016-08-08 16:34:03 +0800

    Update Q121.cpp

--- Q121.cpp
+++ Q121.cpp
@@ -24,3 +24,19 @@ public:
         return profit;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty())
+            return 0;
+            
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        for(int i=0;i<prices.size();++i) {
+            minPrice = min(minPrice,prices[i]);
+            maxProfit = max(maxProfit,prices[i]-minPrice);
+        }
+        return maxProfit;
+    }
+};
