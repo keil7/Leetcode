e8879edef5d2f3dc5689be56b955c2a49a7ccd82
keil7 <mcspro163@gmail.com>
2016-09-02 17:07:40 +0800

    Create Q020.cpp

---
+++ Q020.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack1;
+        for(auto c : s) {
+            if(c=='('||c=='{'||c=='[')
+                stack1.push(c);
+            else {
+                if(stack1.empty())
+                    return false;
+                else if(stack1.top()=='('&&c!=')')
+                    return false;
+                else if(stack1.top()=='{'&&c!='}')
+                    return false;
+                else if(stack1.top()=='['&&c!=']')
+                    return false;
+                else
+                    stack1.pop();
+            }
+        }
+        if(stack1.empty())
+            return true;
+        else
+            return false;
+    }
+};
