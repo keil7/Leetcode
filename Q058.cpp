f7ca79f8d5e9c9fbd09c201c848f1068f32621b3
keil7 <mcspro163@gmail.com>
2016-09-05 19:35:46 +0800

    Create Q058.cpp

---
+++ Q058.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        istringstream is(s);
+        string word;
+        while(is>>word) {}
+        
+        return word.length();
+    }
+};
